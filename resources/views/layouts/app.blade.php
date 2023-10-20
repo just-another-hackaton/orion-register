cb03648e4a36a3e77bab8de1a804edae594d49ed
Tjoosten <Topairy@gmail.com>
2023-10-20 21:52:18 +0200

    WIP

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -61,6 +61,12 @@
                     @csrf {{-- Form field protection --}}
                 </form>
 
+                <li class="nav-item">
+                    <a href="" class="nav-link">
+                        <x-heroicon-o-arrows-right-left class="icon icon-navbar mr-1"/> {{ __('switch to kiosk') }}
+                    </a>
+                </li>
+
                 <li class="nav-item dropdown">
                     <a href="#" class="nav-link dropdown-toggle" id="accountDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <x-heroicon-o-user-circle class="icon icon-navbar mr-1"/> {{ auth()->user()->name }}
@@ -97,6 +103,34 @@
             <nav class="nav nav-underline">
                 <a class="nav-link {{ active('/') }}" href="{{ url('/') }}">
                     <x-heroicon-o-home class="icon icon-subnav" /> {{ __('Dashboard') }}
+                </a>
+
+                <a class="nav-link" href="">
+                    <x-heroicon-o-users class="icon icon-subnav" /> {{ __('Casualties') }}
+                </a>
+
+                <a class="nav-link" href="">
+                    <x-heroicon-o-users class="icon icon-subnav" /> {{ __('Squadrons') }}
+                </a>
+
+                <a class="nav-link" href="">
+                    <x-heroicon-o-users class="icon icon-subnav" /> {{ __('Airforces') }}
+                </a>
+
+                <a class="nav-link" href="">
+                    <x-heroicon-o-paper-airplane class="icon icon-subnav"/> {{ __('Airframes') }}
+                </a>
+
+                <a class="nav-link" href="">
+                    <x-heroicon-o-circle-stack class="icon icon-subnav"/> {{ __('Incidents') }}
+                </a>
+
+                <a class="nav-link" href="">
+                    <x-heroicon-o-list-bullet class="icon icon-subnav" /> {{ __('Memorials') }}
+                </a>
+
+                <a class="nav-link" href="">
+                    <x-heroicon-o-list-bullet class="icon icon-subnav" /> {{ __('Decorations') }}
                 </a>
             </nav>
         </div>
