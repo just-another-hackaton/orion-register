3d13bcfad6a67e257e3737e5d171ecf004a62649
Tjoosten <Topairy@gmail.com>
2023-10-22 00:16:54 +0200

    Implement the subnav item

--- resources/views/layouts/partials/_kiosk-subnav.blade.php
+++ resources/views/layouts/partials/_kiosk-subnav.blade.php
@@ -1,5 +1,9 @@
 <nav class="nav nav-underline">
-    <a class="nav-link {{ active('/') }}" href="{{ url('/') }}">
+    <a class="nav-link {{ active('kiosk') }}" href="{{ url('kiosk') }}">
         <x-heroicon-o-home class="icon icon-subnav" /> {{ __('Dashboard') }}
+    </a>
+
+    <a class="nav-link {{ active('kiosk.user-management.*') }}" href="{{ route('kiosk.user-management.index') }}">
+        <x-heroicon-o-user-group class="icon icon-subnav" /> {{ __('Users') }}
     </a>
 </nav>
