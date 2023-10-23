ec5d06f51a8948a6922c0b5db23edc998325f1d0
Tim Joosten <Tjoosten@users.noreply.github.com>
2023-10-23 21:26:04 +0200

    Implement authorization

--- app/Actions/UserManagement/RegisterUser.php
+++ app/Actions/UserManagement/RegisterUser.php
@@ -6,11 +6,14 @@ namespace App\Actions\UserManagement;
 
 use App\DataObjects\UserDataObject;
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 final class RegisterUser
 {
     public function handle(UserDataObject $userDataObject): User
     {
-
+        return DB::transaction(
+            fn (): User => User::query()->create($userDataObject->toArray())
+        );
     }
 }
--- app/Http/Controllers/UserManagement/CreateNewUserController.php
+++ app/Http/Controllers/UserManagement/CreateNewUserController.php
@@ -7,16 +7,23 @@ namespace App\Http\Controllers\UserManagement;
 use App\Actions\UserManagement\RegisterUser;
 use App\Enums\Users\UserGroup;
 use App\Http\Requests\Users\CreateUserRequest;
+use App\Models\User;
+use App\Policies\UserPolicy;
 use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Http\RedirectResponse;
 use Spatie\RouteAttributes\Attributes\Get;
 use Spatie\RouteAttributes\Attributes\Post;
 
 final readonly class CreateNewUserController
 {
+    use AuthorizesRequests;
+
     #[Get(uri: 'user-management/create', name: 'kiosk.user-management.create')]
     public function create(): Renderable
     {
+        $this->authorize(UserPolicy::Create, User::class);
+
         return view('user-management.create', ['userGroups' => UserGroup::cases()]);
     }
 
