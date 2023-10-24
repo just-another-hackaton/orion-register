248a5acd6fd5de22752eb23e7bef83666b9a1e69
Tjoosten <Topairy@gmail.com>
2023-10-25 00:12:13 +0200

    Update

--- resources/views/user-management/_partials/sidebar.blade.php
+++ resources/views/user-management/_partials/sidebar.blade.php
@@ -2,4 +2,10 @@
     <a href="{{ route('kiosk.user-management.view', $user) }}" class="{{ active('kiosk.user-management.view') }} list-group-item list-group-item-action">
         <x-heroicon-o-information-circle class="icon icon-subnav"/> {{ __('General information') }}
     </a>
+    <a href="{{ route('kiosk.user-management.update', $user) }}" class="{{ active('kiosk.user-management.update') }} list-group-item list-group-item-action">
+        <x-heroicon-o-pencil-square class="icon icon-subnav"/> {{ __('Edit information') }}
+    </a>
+    <a href="" class="list-group-item list-group-item-action">
+        <x-heroicon-o-trash class="icon icon-subnav text-danger"/> {{ __('Delete user') }}
+    </a>
 </div>
